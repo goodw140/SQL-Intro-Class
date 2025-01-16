7c4fe5450666a846ad08f35e3bdc73845ccd7dbf
Ian Goodwin <Ian.goodwin@kellogg.northwestern.edu>
2025-01-16 02:58:52 +0000

    completed 4-1

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -3,4 +3,7 @@
 -- Expected result:
 -- 2935
 
-
+SELECT players.first_name , players.last_name , SUM(stats.hits) 
+FROM stats 
+INNER JOIN players ON players.id = stats.player_id 
+WHERE players.first_name = "Barry" AND players.last_name = "Bonds";
